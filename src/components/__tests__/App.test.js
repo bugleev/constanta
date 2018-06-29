71e7c37a0551bbf55a862067a52796d3dc201720
Andrey <bugol8@yandex.ru>
2018-06-29 10:54:41 +0300

    test skipped added

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { render, Simulate } from "react-testing-library";
+import { render, Simulate, cleanup } from "react-testing-library";
 import EmployeeList from "../EmployeeList";
 import EditMenu from "../EditMenu";
 import App from "./../../containers/App";
 import generateEmployees from "../../helpers/generateEmployees";
 
-test("Employeelist renders correct number of items", () => {
+afterEach(cleanup);
+test.skip("Employeelist renders correct number of items", () => {
   const data = generateEmployees(4);
   const { container } = render(<EmployeeList data={data} />);
   const parentNode = container.querySelector(".employee-ul");
   expect(parentNode.childNodes.length).toBe(data.length);
 });
-test("EditMenu renders correct number of items", () => {
+test.skip("EditMenu renders correct number of items", () => {
   const func = jest.fn;
   const data = generateEmployees(3);
   const { container } = render(
@@ -21,7 +22,7 @@ test("EditMenu renders correct number of items", () => {
   expect(parentNode.childNodes.length).toBe(data.length + 1);
 });
 
-test("opens menu on button click", () => {
+test.skip("opens menu on button click", () => {
   const { container, getByText } = render(<App />);
   const button = getByText("Open Menu");
   Simulate.click(button);
@@ -29,7 +30,7 @@ test("opens menu on button click", () => {
   expect(Object.values(menuNode.classList)).toContain("open");
 });
 
-test("adds item on 'add' button click", () => {
+test.skip("adds item on 'add' button click", () => {
   const { container, getByText } = render(<App />);
   const button = getByText("Add item");
   const menuitemsBefore = container.querySelector(".menu-wrapper").children
@@ -40,7 +41,7 @@ test("adds item on 'add' button click", () => {
   expect(menuitemsBefore).not.toEqual(menuitemsAfter);
 });
 
-test("removes item from the list on 'remove' button click", () => {
+test.skip("removes item from the list on 'remove' button click", () => {
   const { container, getAllByText } = render(<App />);
   const button = getAllByText("remove")[0];
   const listitemsBefore = container.querySelector(".employee-ul").children
@@ -51,7 +52,7 @@ test("removes item from the list on 'remove' button click", () => {
   expect(listitemsBefore).not.toEqual(listitemsAfter);
 });
 
-test("text in the list item changes when text in menu item is changed", () => {
+test.skip("text in the list item changes when text in menu item is changed", () => {
   const { getAllByTestId } = render(<App />);
   const menuInput = getAllByTestId("menu-input")[1];
   const listText = getAllByTestId("list-text")[0];
